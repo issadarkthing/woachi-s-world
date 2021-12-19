8021770252b3c48dd0c9f6f330c652fdc366110a
raziman <raziman.terra@gmail.com>
2021-12-19 16:18:17 +0800

    add Help command

---
+++ src/commands/Help.ts
@@ -0,0 +1,38 @@
+import { Command } from "@jiman24/commandment";
+import { Message, MessageEmbed } from "discord.js";
+
+export default class extends Command {
+  name = "help";
+  aliases = ["h"];
+  description = "show all commands and it's description";
+
+  async exec(msg: Message) {
+    const commands = this.commandManager.commands.values();
+
+    let helpText = "";
+    const done = new Set<string>();
+
+    for (const command of commands) {
+
+      if (command.disable)
+        continue;
+
+      if (done.has(command.name)) {
+        continue
+      } else {
+        done.add(command.name);
+      }
+
+      helpText += 
+        `\n**${command.name}**: \`${command.description || "none"}\``;
+
+    }
+
+    const embed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setTitle("Help")
+      .setDescription(helpText)
+
+    msg.channel.send({ embeds: [embed] });
+  }
+}
